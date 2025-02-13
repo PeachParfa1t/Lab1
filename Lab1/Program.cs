166a223e790130132432a1396ad00b651ac7e853
peachparfa1t <filatova7891@gmail.com>
2025-02-13 16:15:46 +0800

    с проверкой ввода

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -6,29 +6,33 @@
     {
         static void Main()
         {
-            Console.Write("Введите шестизначное число: ");
-            if (int.TryParse(Console.ReadLine(), out int number) && number >= 100000 && number <= 999999)
-            {
-                // Разбираем число на цифры
-                int firstDigit = number / 100000;         // первая цифра
-                int secondDigit = (number / 10000) % 10;  // вторая цифра
-                int thirdDigit = (number / 1000) % 10;    // третья цифра
-                int fourthDigit = (number / 100) % 10;    // четвёртая цифра
-                int fifthDigit = (number / 10) % 10;      // пятая цифра
-                int sixthDigit = number % 10;            // шестая цифра
+            int number;
 
-                int firstSum = firstDigit + secondDigit + thirdDigit;
-                int secondSum = fourthDigit + fifthDigit + sixthDigit;
-
-                if (firstSum == secondSum)
-                    Console.WriteLine("Число счастливое!");
-                else
-                    Console.WriteLine("Число не является счастливым.");
-            }
-            else
+            while (true)
             {
+                Console.Write("Введите шестизначное число: ");
+
+                if (int.TryParse(Console.ReadLine(), out number) && number >= 100000 && number <= 999999)
+                    break;
+
                 Console.WriteLine("Ошибка: введите корректное шестизначное число.");
             }
+
+            int firstDigit = number / 100000;         
+            int secondDigit = (number / 10000) % 10;
+            int thirdDigit = (number / 1000) % 10;
+            int fourthDigit = (number / 100) % 10;
+            int fifthDigit = (number / 10) % 10;
+            int sixthDigit = number % 10;   
+
+            int firstSum = firstDigit + secondDigit + thirdDigit;
+            int secondSum = fourthDigit + fifthDigit + sixthDigit;
+
+            if (firstSum == secondSum)
+                Console.WriteLine("Число счастливое!");
+            else
+                Console.WriteLine("Число не является счастливым.");
         }
     }
+
 }
