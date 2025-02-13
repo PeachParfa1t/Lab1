9798626a1152c8369bd802bc3d92a7aa08c4a82f
peachparfa1t <filatova7891@gmail.com>
2025-02-13 21:24:56 +0800

    Задание 2 Декомпозиция для тестов

--- program2/Program.cs
+++ program2/Program.cs
@@ -2,6 +2,26 @@
 {
     using System;
 
+    class GradeCounter
+    {
+        public int Count5 { get; private set; } = 0;
+        public int Count4 { get; private set; } = 0;
+        public int Count3 { get; private set; } = 0;
+        public int Count2 { get; private set; } = 0;
+
+        public bool ProcessGrade(int grade)
+        {
+            switch (grade)
+            {
+                case 5: Count5++; return true;
+                case 4: Count4++; return true;
+                case 3: Count3++; return true;
+                case 2: Count2++; return true;
+                default: return false;
+            }
+        }
+    }
+
     class Program
     {
         static void Main()
@@ -10,28 +30,27 @@
             int n = int.Parse(Console.ReadLine());
             int[] grades = new int[n];
 
-            int count5 = 0, count4 = 0, count3 = 0, count2 = 0;
+            GradeCounter gradeCounter = new GradeCounter();
 
             for (int i = 0; i < n; i++)
             {
                 Console.Write($"Введите оценку ученика {i + 1}: ");
-                grades[i] = int.Parse(Console.ReadLine());
+                int grade = int.Parse(Console.ReadLine());
 
-                switch (grades[i])
+                if (!gradeCounter.ProcessGrade(grade))
                 {
-                    case 5: count5++; break;
-                    case 4: count4++; break;
-                    case 3: count3++; break;
-                    case 2: count2++; break;
-                    default: Console.WriteLine("Ошибка! Введите оценку от 2 до 5."); i--; break;
+                    Console.WriteLine("Ошибка! Введите оценку от 2 до 5.");
+                    i--;
                 }
             }
 
-            Console.WriteLine($"Количество пятерок: {count5}");
-            Console.WriteLine($"Количество четверок: {count4}");
-            Console.WriteLine($"Количество троек: {count3}");
-            Console.WriteLine($"Количество двоек: {count2}");
+            Console.WriteLine($"Количество пятерок: {gradeCounter.Count5}");
+            Console.WriteLine($"Количество четверок: {gradeCounter.Count4}");
+            Console.WriteLine($"Количество троек: {gradeCounter.Count3}");
+            Console.WriteLine($"Количество двоек: {gradeCounter.Count2}");
         }
     }
+
+
 
 }
